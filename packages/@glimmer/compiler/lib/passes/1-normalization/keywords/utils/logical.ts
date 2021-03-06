3f69ca51b9755b58ce95d47ee023f15fbec4092c
scott <snewcomer24@gmail.com>
2021-03-06 14:50:44 -0600

    Add not keyword

--- packages/@glimmer/compiler/lib/passes/1-normalization/keywords/utils/logical.ts
+++ packages/@glimmer/compiler/lib/passes/1-normalization/keywords/utils/logical.ts
@@ -65,3 +65,43 @@ export const orKeyword: KeywordDelegate<
   assert: assertOrKeyword,
   translate: translateOrKeyword,
 };
+
+function assertNotKeyword(node: GenericKeywordNode): Result<ASTv2.ExpressionNode> {
+  let {
+    args: { named, positional },
+  } = node;
+
+  if (named && !named.isEmpty()) {
+    return Err(generateSyntaxError(`(not) does not take any named arguments`, node.loc));
+  }
+
+  if (positional?.size > 1) {
+    return Err(generateSyntaxError(`(not) only accepts 1 argument`, node.loc));
+  }
+
+  let value = positional?.nth(0);
+
+  if (!value) {
+    return Err(generateSyntaxError(`(not) requires 1 argument`, node.loc));
+  }
+
+  return Ok(value);
+}
+
+function translateNotKeyword(
+  { node, state }: { node: ASTv2.CallExpression; state: NormalizationState },
+  value: ASTv2.ExpressionNode
+): Result<mir.Not> {
+  return VISIT_EXPRS.visit(value, state).mapOk(
+    (result) => new mir.Not({ value: result, loc: node.loc })
+  );
+}
+
+export const notKeyword: KeywordDelegate<
+  ASTv2.CallExpression | ASTv2.AppendContent,
+  ASTv2.ExpressionNode,
+  mir.Not
+> = {
+  assert: assertNotKeyword,
+  translate: translateNotKeyword,
+};
